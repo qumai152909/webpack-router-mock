5f3bd479ff0ae1a1efb0c5a2b82ef6f36ace07d8
sunyingying23 <sunyingying23@jd.com>
2020-11-19 20:53:30 +0800

    优化目录结构

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './index.css';
 
 export default function Home() {
   return (
     <div className="home-wrapper">
       Home
+      <div className="flower-block">flower</div>
     </div>
   );
 }
