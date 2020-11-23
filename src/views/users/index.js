5e437f3c99014e0d1418f33eb66ec9419829446b
Yingying Sun <1079808037@qq.com>
2020-11-23 23:03:05 +0800

    修改打包方式

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import { add } from '../../utils/add';
+
 export default function User() {
+  console.log('add: ', add(2, 4));
+  
   return (
     <div className="user-wrapper">
       User
